79eb65b3d1a46c2450104234328270d944a49665
darkisabove <rafhaelhailar@yahoo.com>
2024-06-18 08:41:26 +0800

    [Update] removed mysqldump package.

--- database/backup.js
+++ database/backup.js
@@ -1,34 +1,34 @@
-import mysqldump from "mysqldump";
 import getFiles from "../lib/getFiles.js";
 import authorize from "../lib/googleAuthorize.js";
 import updateFile from "../lib/updateFile.js";
+import uploadFile from "../lib/uploadFile.js";
+import fs from 'fs';
+import createADump from "./createADump.js";
 
 const backUpFilePath = './database/database.backup.sql';
 
 async function createABackup() {
-    return mysqldump({
-        connection: {
-            host: 'localhost',
-            user: 'root',
-            password: '',
-            database: 'info_management',
-        },
-        dumpToFile: backUpFilePath,
-    });
+    return createADump(backUpFilePath);
 }
 
 async function sendFileToDrive() {
     authorize().then(async(jwt) => {
         try {
-            const files = await getFiles(jwt);
-            const file = files[0];
+            const files = await getFiles(jwt);     
 
-            const fileId = file.id;
-
-            updateFile(jwt,fileId,{
-                file: backUpFilePath,
-                mimeType: "application/sql"
-            })
+            if (files.length === 0) {
+                uploadFile(jwt,{
+                    file: backUpFilePath,
+                    mimeType: "application/sql"
+                });
+            } else {
+                const file = files[0];
+                const fileId = file.id;
+                updateFile(jwt,fileId,{
+                    file: backUpFilePath,
+                    mimeType: "application/sql"
+                })
+            };
         } catch (e) {
             console.log(e)
         }
@@ -37,7 +37,19 @@ async function sendFileToDrive() {
 
 function backup() {
     createABackup().then(() => {
-        sendFileToDrive();
+        fs.readFile(backUpFilePath, 'utf8', function(err, data){
+            if (err) throw err;
+        
+            // Prepend data to the file content
+            const fileContent = "DROP DATABASE IF EXISTS info_management;\nCREATE DATABASE info_management;\nUSE info_management;\n" + data;
+        
+            // Write the new content back to the file
+            fs.writeFile(backUpFilePath, fileContent, 'utf8', function(err) {
+                if (err) throw err;
+                console.log('File updated successfully!');
+                sendFileToDrive();
+            });
+        });
     }).catch(error => console.log(error));
 }
 
